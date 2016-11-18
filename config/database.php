e7785579ec1d51ecfef14885884fa4b826e00108
Markus Eiglsperger <markus.eiglsperger@htwg-konstanz.de>
2016-11-18 23:33:48 +0100

    Technicial update to database config.

--- config/database.php
+++ config/database.php
@@ -54,15 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'garden'),
-            'username' => env('DB_USERNAME', 'meiglspe'),
-            'password' => env('DB_PASSWORD', 'weteadmin'),
-            /**
-             *            'database' => env('DB_DATABASE', 'garden'),
-            'username' => env('DB_USERNAME', 'gardenapp'),
-            'password' => env('DB_PASSWORD', 'M03ghhttwZVkTsI2'), */
+            'host' => env('DB_HOST', ''),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
